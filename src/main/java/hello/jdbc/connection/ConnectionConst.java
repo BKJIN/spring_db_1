690705c0cabce6f2b73cc4fb4ed98786a439ca6c
Jimoo An <jmdh1004@gmail.com>
2023-07-24 23:47:36 +0900

    jdbc

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package hello.jdbc.connection;
+
+//생성 못하게 추상클래스
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String Username = "sa";
+    public static final String PASSWORD = "";
+}
