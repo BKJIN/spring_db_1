11fb1824f198ee44a2e790fdd844f8b90062dcaf
Jimoo An <jmdh1004@gmail.com>
2023-07-25 16:48:43 +0900

    Connection pool & Datasource

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -14,7 +14,7 @@ public class DBConnectionUtil {
     //JDBC 표준 인터페이스가 제공하는 Connection
     public static Connection getConnection() {
         try {
-            Connection connection = DriverManager.getConnection(URL, Username, PASSWORD);
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             log.info("get connection={}, class={}", connection, connection.getClass());
             return connection;
         } catch (SQLException e) {
